cc0b482f9426ab52644535ddf2cc9122b30c7e23
sumin <smkl1004@snu.ac.kr>
2025-09-28 17:02:41 +0900

    feat: json파일로 데이터 저장 후 fetch 요청을 통해 화면에 가져오기

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import { initModal } from "./modules/modal.js";
 import { initCarousel } from "./modules/carousel.js";
+import { loadMovies } from "./modules/movieLoader.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  initModal();
+document.addEventListener("DOMContentLoaded", async () => {
+  await loadMovies();
   initCarousel();
+  initModal();
 });
