caead3dccd5d88bc1a4fa780b87b02be17f2a986
kimnamheeee <nami200284@snu.ac.kr>
2025-09-28 16:16:30 +0900

    🧹 chore: remove unused file

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import './components/appButton/appButton.ts';
-import './components/arrowButton/arrowButton.ts';
-import './components/faqAccordion/faqAccordion.ts';
-import './components/featureCard/featureCard.ts';
-import './components/languageSelect/languageSelect.ts';
-import './components/movieCard/movieCard.ts';
-import './components/movieModal/movieModal.ts';
-
-import './services/movieService.ts';
