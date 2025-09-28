f76241e02758258b4a19ee41ec47e99193886fc7
kimnamheeee <nami200284@snu.ac.kr>
2025-09-28 17:00:34 +0900

    ✨ feat(movielist): streamline movie loading, rendering logic

--- src/components/movieList/movieList.ts
+++ src/components/movieList/movieList.ts
@@ -5,65 +5,50 @@ import type { Movie } from '../../api/getMovies';
 class MovieList extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-        <suspense-container>
-          <div slot="fallback" class="top-contents__container">
-            <arrow-button direction="left"></arrow-button>
-            <ol class="top-contents__list">
-              ${Array.from({ length: 10 })
-                .map(
-                  () => `
-                    <li class="top-contents__item">
-                      <movie-skeleton-card></movie-skeleton-card>
-                    </li>`
-                )
-                .join('')}
-            </ol>
-            <arrow-button direction="right"></arrow-button>
-          </div>
-  
-          <async-component slot="content" id="movies-loader">
-            <div class="top-contents__container">
-              <arrow-button direction="left"></arrow-button>
-              <ol class="top-contents__list" id="movies-list"></ol>
-              <arrow-button direction="right"></arrow-button>
-            </div>
-          </async-component>
-        </suspense-container>
-      `;
+      <suspense-container>
+        <div slot="fallback" class="top-contents__container">
+        <arrow-button direction="left"></arrow-button>
+        <ol class="top-contents__list">
+          ${Array.from({ length: 10 })
+            .map(() => '<movie-skeleton-card></movie-skeleton-card>')
+            .join('')}
+          </ol>
+        <arrow-button direction="right"></arrow-button>
+        </div>
+        <async-component slot="content" id="movies-loader"></async-component>
+      </suspense-container>
+    `;
 
     this.loadMovies();
   }
 
   async loadMovies() {
     const moviesLoader = this.querySelector('#movies-loader') as AsyncComponent;
-
     if (!moviesLoader) return;
 
-    const moviesPromise = getTrendingMovies().then(movies => {
-      this.renderMovies(movies);
-      return movies;
+    moviesLoader.dataPromise = getTrendingMovies().then((movies: Movie[]) => {
+      return `
+        <div class="top-contents__container">
+        <arrow-button direction="left"></arrow-button>
+          <ol class="top-contents__list">
+            ${movies
+              .map(
+                (movie, idx) => `
+                  <li class="top-contents__item">
+                    <movie-card
+                      src="${movie.thumbnail}"
+                      alt="${movie.title}"
+                      title="${movie.title}"
+                      rank="${idx + 1}"
+                    ></movie-card>
+                  </li>`
+              )
+              .join('')}
+          </ol>
+          <arrow-button direction="right"></arrow-button>
+        </div>
+      `;
     });
-
-    moviesLoader.dataPromise = moviesPromise;
-  }
-
-  renderMovies(movies: Movie[]) {
-    const moviesList = this.querySelector('#movies-list');
-    if (!moviesList) return;
-
-    moviesList.innerHTML = movies
-      .map(
-        (movie, idx) => `
-          <li class="top-contents__item">
-            <movie-card 
-              src="${movie.thumbnail}" 
-              alt="${movie.title}" 
-              title="${movie.title}" 
-              rank="${idx + 1}">
-            </movie-card>
-          </li>`
-      )
-      .join('');
   }
 }
 
