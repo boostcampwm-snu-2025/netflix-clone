75be6ae0da9b21c6b84db0dd303b5f1317e30ba2
kimnamheeee <nami200284@snu.ac.kr>
2025-09-28 16:59:23 +0900

    ✨ feat(utils): add wrapPromise utility for handling promise states

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -143,3 +143,28 @@ export function updateSlotData(
     }
   });
 }
+
+export function wrapPromise<T>(promise: Promise<T>) {
+  let status: 'pending' | 'success' | 'error' = 'pending';
+  let result: T;
+  let error: any;
+
+  const suspender = promise.then(
+    r => {
+      status = 'success';
+      result = r;
+    },
+    e => {
+      status = 'error';
+      error = e;
+    }
+  );
+
+  return {
+    read(): T {
+      if (status === 'pending') throw suspender;
+      if (status === 'error') throw error;
+      return result!;
+    },
+  };
+}
