fd3c78c02713d9bf23587285a01a1c1682e63975
kimnamheeee <nami200284@snu.ac.kr>
2025-09-28 15:39:31 +0900

    ✨ feat(entry-client): add imports for custom elements

--- src/entry-client.ts
+++ src/entry-client.ts
@@ -1,5 +1,14 @@
 import './components/suspenseContainer/suspenseContainer.ts';
 import './components/asyncComponent/asyncComponent.ts';
+
+import './components/appButton/appButton.ts';
+import './components/arrowButton/arrowButton.ts';
+import './components/faqAccordion/faqAccordion.ts';
+import './components/featureCard/featureCard.ts';
+import './components/languageSelect/languageSelect.ts';
+import './components/movieCard/movieCard.ts';
+import './components/movieModal/movieModal.ts';
+
 import { worker } from './mocks/browser.ts';
 
 await worker.start({
