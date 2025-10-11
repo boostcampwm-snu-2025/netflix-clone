7cebf35c62791230ac5a002d2efb16e62a7e14e8
kimnamheeee <nami200284@snu.ac.kr>
2025-10-11 17:14:13 +0900

    ✨ feat(moviegrid): implement scroll using intersectionobserver

--- src/components/movieGrid/movieGrid.ts
+++ src/components/movieGrid/movieGrid.ts
@@ -3,13 +3,22 @@ import {
   searchMovies,
   type MoviesResponse,
   type SearchResponse,
+  type SearchMovie,
+  getNextPage,
 } from '../../api/movies.ts';
 import type AsyncComponent from '../asyncComponent/asyncComponent.ts';
 import { loadTemplate } from '../utils.ts';
 import './style.css';
+import InfiniteScrollObserver from './infiniteScrollObserver.ts';
 
 class MovieGrid extends HTMLElement {
   private abortController: AbortController | null = null;
+  private infiniteScrollObserver: InfiniteScrollObserver | null = null;
+  private lastResponse: SearchResponse | MoviesResponse | null = null;
+  private query: string = '';
+  private hasNextPage: boolean = false;
+  private allMovies: SearchMovie[] = [];
+
   constructor() {
     super();
   }
@@ -17,6 +26,12 @@ class MovieGrid extends HTMLElement {
   async connectedCallback(): Promise<void> {
     const template = await loadTemplate('./template.html', import.meta.url);
     this.appendChild(template.content.cloneNode(true));
+    this.infiniteScrollObserver = new InfiniteScrollObserver(
+      '#infinite-scroll-observer',
+      () => {
+        this.loadNextPage();
+      }
+    );
 
     this.loadMovies();
   }
@@ -24,6 +39,15 @@ class MovieGrid extends HTMLElement {
   async loadMovies(query: string = '', page: number = 1) {
     const moviesLoader = this.querySelector('#movies-loader') as AsyncComponent;
     if (!moviesLoader) return;
+
+    const scrollContainer = document.querySelector('.search-results__content');
+
+    const savedScrollTop =
+      page > 1 && scrollContainer ? scrollContainer.scrollTop : 0;
+
+    if (page === 1) {
+      this.allMovies = [];
+    }
 
     if (this.abortController) this.abortController.abort();
     this.abortController = new AbortController();
@@ -40,10 +64,20 @@ class MovieGrid extends HTMLElement {
 
     moviesLoader.dataPromise = apiCall
       .then((response: MoviesResponse | SearchResponse) => {
-        const movies =
+        this.hasNextPage = response.hasNextPage;
+        this.lastResponse = response;
+        const newMovies =
           'movies' in response ? response.movies : response.results;
 
-        if (movies.length === 0) {
+        this.allMovies = [...this.allMovies, ...newMovies];
+
+        if (page > 1 && scrollContainer) {
+          requestAnimationFrame(() => {
+            scrollContainer.scrollTop = savedScrollTop;
+          });
+        }
+
+        if (this.allMovies.length === 0) {
           return `<div class="movie-grid empty">
           <div class="movie-grid__item">
             <h3 class="movie-grid__title">영화를 찾을 수 없습니다.</h3>
@@ -53,7 +87,7 @@ class MovieGrid extends HTMLElement {
 
         return `
         <div class="movie-grid">
-          ${movies
+          ${this.allMovies
             .map(
               movie => `
             <div class="movie-grid__item">
@@ -71,7 +105,6 @@ class MovieGrid extends HTMLElement {
       `;
       })
       .catch(error => {
-        console.log(error.name);
         if (error.name === 'AbortError') {
           return '<div></div>';
         } else if (error.name === 'TimeoutError') {
@@ -84,7 +117,27 @@ class MovieGrid extends HTMLElement {
       });
   }
 
+  async loadNextPage() {
+    if (!this.lastResponse || !this.hasNextPage) return;
+    const nextPage = await getNextPage(
+      this.lastResponse,
+      this.abortController?.signal as AbortSignal,
+      'movies',
+      this.query
+    );
+    this.lastResponse = nextPage;
+
+    if (
+      nextPage &&
+      this.hasNextPage &&
+      nextPage.currentPage < nextPage.totalPages
+    ) {
+      this.loadMovies(this.query, nextPage.currentPage + 1);
+    }
+  }
+
   search(query: string) {
+    this.query = query;
     this.loadMovies(query, 1);
   }
 }
