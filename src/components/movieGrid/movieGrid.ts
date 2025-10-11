2c6410cb63d1c0c9358b2b61dfc1c779e2c1515c
kimnamheeee <nami200284@snu.ac.kr>
2025-10-11 14:49:59 +0900

    ✨ feat(moviegrid): add movie fetching logic to movieGrid

--- src/components/movieGrid/movieGrid.ts
+++ src/components/movieGrid/movieGrid.ts
@@ -1,3 +1,5 @@
+import { getMovies, type MoviesResponse } from '../../api/movies.ts';
+import type AsyncComponent from '../asyncComponent/asyncComponent.ts';
 import { loadTemplate } from '../utils.ts';
 import './style.css';
 
@@ -9,6 +11,33 @@ class MovieGrid extends HTMLElement {
   async connectedCallback(): Promise<void> {
     const template = await loadTemplate('./template.html', import.meta.url);
     this.appendChild(template.content.cloneNode(true));
+
+    this.loadMovies();
+  }
+
+  async loadMovies() {
+    const moviesLoader = this.querySelector('#movies-loader') as AsyncComponent;
+    if (!moviesLoader) return;
+    moviesLoader.dataPromise = getMovies().then((response: MoviesResponse) => {
+      return `
+        <div class="movie-grid">
+          ${response.movies
+            .map(
+              movie => `
+            <div class="movie-grid__item">
+              <img
+                class="movie-grid__image"
+                src="${movie.thumbnail}"
+                alt="${movie.title}"
+              />
+              <h3 class="movie-grid__title">${movie.title}</h3>
+            </div>
+          `
+            )
+            .join('')}
+        </div>
+      `;
+    });
   }
 }
 
